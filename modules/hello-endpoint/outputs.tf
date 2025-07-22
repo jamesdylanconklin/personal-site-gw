2a3c0a8293076476297383b0c75b5737bb807df6
James Conklin <jamesdylanconklin@gmail.com>
2025-07-22 11:46:50 -0700

    Pull path from created resource

--- modules/hello-endpoint/outputs.tf
+++ modules/hello-endpoint/outputs.tf
@@ -7,3 +7,8 @@ output "resource_id" {
   description = "The ID of the API Gateway resource"
   value       = aws_api_gateway_resource.hello.id
 }
+
+output "resource_path" {
+  description = "The full path of this resource"
+  value       = aws_api_gateway_resource.hello.path
+}
